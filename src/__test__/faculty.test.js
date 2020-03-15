d5672abc52edccb1f1b112ec755b601904de9dcb
srhr17 <srhr1999@gmail.com>
2020-03-15 19:07:00 +0400

    ..

--- src/__test__/faculty.test.js
+++ src/__test__/faculty.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { shallow, configure } from 'enzyme';
 
-import Facultytimetable from './Facultytimetable';
+import Facultytimetable from '../components/Facultytimetable.jsx';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
