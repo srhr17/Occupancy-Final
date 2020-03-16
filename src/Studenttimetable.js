6d3b379e93891e9cf350413a30dd964d00a3ff13
srhr17 <srhr1999@gmail.com>
2020-03-16 06:41:30 +0400

    Update Studenttimetable.js

--- src/Studenttimetable.js
+++ src/Studenttimetable.js
@@ -9,115 +9,138 @@ import React from 'react';
 //import 'react-inputs-validation/lib/react-inputs-validation.min.css';
 
 function Header() {
-  // Import result is the URL of your image
-  //return <img src={logo} alt="logo" />;
+	// Import result is the URL of your image
+	//return <img src={logo} alt="logo" />;
 }
 // export default Header;
-class Studenttimetable extends React.Component{
-constructor() {
-   super();
-   this.state = {
-  
-     section: "",
-     sem: "",
+class Studenttimetable extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			section: '',
+			sem: ''
+			//  ,
 
-     errors: {
-       
-       section: " ",
-      sem: " "
-     }
-   };
- }
+			//  errors: {
 
- validateForm = errors => {
-   let valid = true;
-   Object.values(errors).forEach(val => val.length > 0 && (valid = false));
-   return valid;
- };
+			//    section: " ",
+			//   sem: " "
+			//  }
+		};
+	}
 
- handleChange = e => {
-   const { name, value } = e.target;
-   let errors = this.state.errors;
-   switch (name) {
-     
-     case "section":
-       errors.section = value === " " ? "Select section" : "";
-       break;
+	//  validateForm = errors => {
+	//    let valid = true;
+	//    Object.values(errors).forEach(val => val.length > 0 && (valid = false));
+	//    return valid;
+	//  };
 
-     case "sem":
-       errors.sem = value === " " ? "Select sem" : "";
-       break;
+	//  handleChange = e => {
+	//    const { name, value } = e.target;
+	//    let errors = this.state.errors;
+	//    switch (name) {
 
-     default:
-       break;
-   }
+	//      case "section":
+	//        errors.section = value === " " ? "Select section" : "";
+	//        break;
 
-   this.setState({ errors, [name]: value });
- };
- 
-render(){
-  return(
-    
-     
-      
-    <div  >
-      
-    <div  >
-      <div style={{backgroundColor:"#ffeecc"}} > 
-      
-      
-        <h1 style={{textAlign: 'center', border: '20px solid white', padding: '20px', backgroundColor: '#b3daff', borderBottom: '20px solid grey',paddingRight:"30px"}}>STUDENT TIMETABLE</h1>
-        
-        <form action="http://localhost:8000/time" method="POST"  style={{border: '3px solid black', textAlign: 'center', padding: '15px', margin: '10px 450px', backgroundColor: 'ivory', borderRadius: '35px',marginTop:"13%",}}>
-  
-        <br/>
-       
-       
-           <label><h3 style={{paddingLeft:'70px',paddingRight:'20px'}}>SEM:</h3></label>
-     
+	//      case "sem":
+	//        errors.sem = value === " " ? "Select sem" : "";
+	//        break;
 
-          <select name="sem" onChange={this.handleChange} style={{borderRadius:'10px',paddingRight:'10px',paddingLeft:'10px',width:'150px'}}>
-                        <option value=" ">Select</option>
-                        <option value="2">2</option>
-                        <option value="4">4</option>
-                        <option value="6">6</option>
-                        <option value="8">8</option>
-                  
-                      </select>
-    
-    
-        <span style={{color: "red"}}>{this.state.errors["sem"]}</span> <br/>
-     
-           <br/>
+	//      default:
+	//        break;
+	//    }
 
-           <br/>
-        
-      
-             
- <label><h3 style={{paddingLeft:'30px',paddingRight:'30px'}}>SECTION:</h3></label>
-   
-   
-         
-                      <select name="section" onChange={this.handleChange} style={{borderRadius:'10px',paddingRight:'10px',paddingLeft:'10px',width:'150px'}}>
-                        <option value=" ">Select</option>
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                        <option value="D">D</option>
-                        <option value="E">E</option>
-                        <option value="F">F</option>
-                      </select>
-    
-        <span style={{color: "red"}}>{this.state.errors["section"]}</span> <br/>
-     
+	//    this.setState({ errors, [name]: value });
+	//  };
 
-       
-<br/>
+	render() {
+		return (
+			<div>
+				<div>
+					<div style={{ backgroundColor: '#ffeecc' }}>
+						<h1
+							style={{
+								textAlign: 'center',
+								border: '20px solid white',
+								padding: '20px',
+								backgroundColor: '#b3daff',
+								borderBottom: '20px solid grey',
+								paddingRight: '30px'
+							}}
+						>
+							STUDENT TIMETABLE
+						</h1>
 
-{/* <input type="submit" name="submit"></input> */}
-
-            <button style={{borderRadius: '50px', width: '150px',backgroundColor:' #80ff80'} } data-testid = "submitbutton" onClick="">SUBMIT</button>
-            {/* <input
+						<form
+							action="http://localhost:8000/time"
+							method="POST"
+							style={{
+								border: '3px solid black',
+								textAlign: 'center',
+								padding: '15px',
+								margin: '10px 450px',
+								backgroundColor: 'ivory',
+								borderRadius: '35px',
+								marginTop: '13%'
+							}}
+						>
+							<br />
+							<label>
+								<h3 style={{ paddingLeft: '70px', paddingRight: '20px' }}>SEM:</h3>
+							</label>
+							<select
+								name="sem"
+								onChange={this.handleChange}
+								style={{
+									borderRadius: '10px',
+									paddingRight: '10px',
+									paddingLeft: '10px',
+									width: '150px'
+								}}
+							>
+								<option value=" ">Select</option>
+								<option value="2">2</option>
+								<option value="4">4</option>
+								<option value="6">6</option>
+								<option value="8">8</option>
+							</select>
+							<span style={{ color: 'red' }}>{this.state.errors['sem']}</span> <br />
+							<br />
+							<br />
+							<label>
+								<h3 style={{ paddingLeft: '30px', paddingRight: '30px' }}>SECTION:</h3>
+							</label>
+							<select
+								name="section"
+								onChange={this.handleChange}
+								style={{
+									borderRadius: '10px',
+									paddingRight: '10px',
+									paddingLeft: '10px',
+									width: '150px'
+								}}
+							>
+								<option value=" ">Select</option>
+								<option value="A">A</option>
+								<option value="B">B</option>
+								<option value="C">C</option>
+								<option value="D">D</option>
+								<option value="E">E</option>
+								<option value="F">F</option>
+							</select>
+							<span style={{ color: 'red' }}>{this.state.errors['section']}</span> <br />
+							<br />
+							{/* <input type="submit" name="submit"></input> */}
+							<button
+								style={{ borderRadius: '50px', width: '150px', backgroundColor: ' #80ff80' }}
+								data-testid="submitbutton"
+								onClick=""
+							>
+								SUBMIT
+							</button>
+							{/* <input
                       data-testid="submit"
                       type="submit"
                       onClick={e => {
@@ -136,25 +159,22 @@ render(){
                              }
                            )}}}
                           ></input> */}
-          </form>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-         
-         
-          </div>
-          </div>
-          </div>
-    );
+						</form>
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-}
-
 
 export default Studenttimetable;
