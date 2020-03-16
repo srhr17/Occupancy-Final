ded121fb0c341dfccc5b90450b789c6140d58ad8
srhr17 <srhr1999@gmail.com>
2020-03-16 12:01:08 +0400

    req

--- src/__test__/studentfree.test.js
+++ src/__test__/studentfree.test.js
@@ -1,50 +1,45 @@
 import React from 'react';
 
-import {shallow, mount , configure} from 'enzyme'
+import { shallow, configure } from 'enzyme';
 //import Studenttimetable from './Studenttimetable';
 import Studentfree from '../studentfree';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-it('validates inputs of student free slots-section',()=>{
-    var wrapper9 = shallow(<Studentfree/>)
-    wrapper9.find('[name="section"]').at(0).simulate('change',{target : {name: 'section' , value: ' '}})
-    wrapper9.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper9.state().errors["section"]).toBe('Select section')
-
-})
+it('validates inputs of student free slots-section', () => {
+	var wrapper9 = shallow(<Studentfree />);
+	wrapper9.find('[name="section"]').at(0).simulate('change', { target: { name: 'section', value: ' ' } });
+	wrapper9.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper9.state().errors['section']).toBe('Select section');
+});
 configure({ adapter: new Adapter() });
-it('validates inputs of student free slots-section',()=>{
-    var wrapper10 = shallow(<Studentfree/>)
+it('validates inputs of student free slots-section', () => {
+	var wrapper10 = shallow(<Studentfree />);
 
+	wrapper10.find('[name="section"]').at(0).simulate('change', { target: { name: 'section', value: 'E' } });
+	wrapper10.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper10.state().errors['section']).toBe('');
+});
 
-    wrapper10.find('[name="section"]').at(0).simulate('change',{target : {name: 'section' , value: 'E'}})
-    wrapper10.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper10.state().errors["section"]).toBe('')
-
-})
-
-it('validates inputs of student free slots-sem',()=>{
-    var wrapper11 = shallow(<Studentfree/>)
-    wrapper11.find('[name="sem"]').at(0).simulate('change',{target : {name: 'sem' , value: ' '}})
-    wrapper11.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper11.state().errors["sem"]).toBe('Select sem')
-})
+it('validates inputs of student free slots-sem', () => {
+	var wrapper11 = shallow(<Studentfree />);
+	wrapper11.find('[name="sem"]').at(0).simulate('change', { target: { name: 'sem', value: ' ' } });
+	wrapper11.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper11.state().errors['sem']).toBe('Select sem');
+});
 configure({ adapter: new Adapter() });
-it('validates inputs of student free slots-sem',()=>{
-    var wrapper12= shallow(<Studentfree/>)
+it('validates inputs of student free slots-sem', () => {
+	var wrapper12 = shallow(<Studentfree />);
 
-
-    wrapper12.find('[name="sem"]').at(0).simulate('change',{target : {name: 'sem' , value: '2'}})
-    wrapper12.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper12.state().errors["sem"]).toBe('')
-
-})
+	wrapper12.find('[name="sem"]').at(0).simulate('change', { target: { name: 'sem', value: '2' } });
+	wrapper12.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper12.state().errors['sem']).toBe('');
+});
--- src/__test__/studenttimetable.test.js
+++ src/__test__/studenttimetable.test.js
@@ -1,55 +1,42 @@
 import React from 'react';
 
-import {shallow, mount , configure} from 'enzyme'
-import Studenttimetable from '../Studenttimetable.js'
+import { shallow, configure } from 'enzyme';
+import Studenttimetable from '../Studenttimetable.js';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-it('validates inputs of student time table-sem',()=>{
-    var wrapper1 = shallow(<Studenttimetable/>)
-    wrapper1.find('[name="sem"]').at(0).simulate('change',{target : {name: 'sem' , value: ' '}})
-    wrapper1.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper1.state().errors["sem"]).toBe('Select sem')
+it('validates inputs of student time table-sem', () => {
+	var wrapper1 = shallow(<Studenttimetable />);
+	wrapper1.find('[name="sem"]').at(0).simulate('change', { target: { name: 'sem', value: ' ' } });
+	wrapper1.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper1.state().errors['sem']).toBe('Select sem');
+});
+it('validates inputs of student time table-sem', () => {
+	var wrapper2 = shallow(<Studenttimetable />);
 
+	wrapper2.find('[name="sem"]').at(0).simulate('change', { target: { name: 'sem', value: '2' } });
+	wrapper2.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1st.js
 
+	// expect(data).toBe(1)
+	expect(wrapper2.state().errors['sem']).toBe('');
+});
+it('validates inputs of student time table-section', () => {
+	var wrapper3 = shallow(<Studenttimetable />);
+	wrapper3.find('[name="section"]').at(0).simulate('change', { target: { name: 'section', value: ' ' } });
+	wrapper3.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper3.state().errors['section']).toBe('Select section');
+});
+it('validates inputs of student time table-section', () => {
+	var wrapper4 = shallow(<Studenttimetable />);
 
-   
-})
-it('validates inputs of student time table-sem',()=>{
-    var wrapper2 = shallow(<Studenttimetable/>)
-
-
-    wrapper2.find('[name="sem"]').at(0).simulate('change',{target : {name: 'sem' , value: '2'}})
-    wrapper2.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1st.js
-    
-    // expect(data).toBe(1)
-    expect(wrapper2.state().errors["sem"]).toBe('')
-})
-it('validates inputs of student time table-section',()=>{
-    var wrapper3 = shallow(<Studenttimetable/>)
-    wrapper3.find('[name="section"]').at(0).simulate('change',{target : {name: 'section' , value: ' '}})
-    wrapper3.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper3.state().errors["section"]).toBe('Select section')
-
-
-
-   
-})
-it('validates inputs of student time table-section',()=>{
-    var wrapper4 = shallow(<Studenttimetable/>)
-   
-    wrapper4.find('[name="section"]').at(0).simulate('change',{target : {name: 'section' , value: 'E'}})
-    wrapper4.find('[data-testid="submitbutton"]').at(0).simulate('click')
-    // var data=1
-    // expect(data).toBe(1)
-    expect(wrapper4.state().errors["section"]).toBe('')
-
-
-
-   
-})
+	wrapper4.find('[name="section"]').at(0).simulate('change', { target: { name: 'section', value: 'E' } });
+	wrapper4.find('[data-testid="submitbutton"]').at(0).simulate('click');
+	// var data=1
+	// expect(data).toBe(1)
+	expect(wrapper4.state().errors['section']).toBe('');
+});
